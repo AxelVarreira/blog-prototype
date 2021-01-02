8d2818751a32d6c26dfe1d21dbb47a0fe2135c29
AxelVarreira <axel.felix.varreira2@gmail.com>
2021-01-02 07:48:56 -0300

    Added post cards and some test files added!

---
+++ frontend/dist/server.js
@@ -0,0 +1,23 @@
+"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _express = require('express'); var _express2 = _interopRequireDefault(_express);
+
+var _nunjucks = require('nunjucks'); var _nunjucks2 = _interopRequireDefault(_nunjucks);
+
+const server = _express2.default.call(void 0, );
+
+const PORT = process.env.PORT || 3333;
+
+_nunjucks2.default.configure("src/views", {
+  express: server,
+  noCache: true,
+});
+
+function homePage(req, res) {
+  return res.render("index.html");
+}
+
+server
+  .use(_express2.default.static("public"))
+  .get("/", homePage)
+  .listen(PORT, () => {
+    console.log(`listening port ${PORT}`);
+  });
